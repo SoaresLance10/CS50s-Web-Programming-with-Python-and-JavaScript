056ac2187073441c761bba1aed522436dc06af5a
Lance Soares <soareslance10@gmail.com>
2020-12-15 15:11:46 +0530

    Added final project

--- final/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Poster, Posts, Comment
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Poster)
-admin.site.register(Posts)
-admin.site.register(Comment)
